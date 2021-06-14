3579257c325c1ae0a0566159821bea4040e7696e
Apoorv Malik <apoorv258@gmail.com>
2021-06-14 14:25:47 +0530

    added web application

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,15 +4,15 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4d0ea861c79b029330b20ead9c96dabc",
-"index.html": "12e25ed56f5c381f03c4deecd9c0108f",
-"/": "12e25ed56f5c381f03c4deecd9c0108f",
+"index.html": "bae49abab1a390b45ac69fdf9487c23b",
+"/": "bae49abab1a390b45ac69fdf9487c23b",
 "main.dart.js": "807d2c0c9d43860714c13a9a215fd966",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "dbf507340f8aab10dca2649ce64ecc7d",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "d9951480e51231c5642276aa452dfcc7",
+"assets/NOTICES": "35d6597bef7ff861fbdb1cdc5f70b453",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
